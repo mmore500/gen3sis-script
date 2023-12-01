751e909185759042ccefac3b6202a935f4f5210d
Matthew Andres Moreno <mmore500.login@gmail.com>
2023-12-01 18:15:50 -0500

    Update run_workflow.R

--- run_workflow.R
+++ run_workflow.R
@@ -1,4 +1,14 @@
 print("run_workflow.R")
-source("create_landscape.R")
-source("create_config.R")
-source("run_simulation.R")
+tryCatch({
+    # Try sourcing the file from the first directory
+    source("gen3sis-script/create_landscape.R")
+    source("gen3sis-script/create_config.R")
+    source("gen3sis-script/run_simulation.R")
+  },
+  error = function(e) {
+    # If sourcing from the first directory fails, source from the alternative directory
+    source("create_landscape.R")
+    source("create_config.R")
+    source("run_simulation.R")
+  }
+)

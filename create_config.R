a533b883f34a40e8ff989af855da6a055cc787d7
Matthew Andres Moreno <mmore500.login@gmail.com>
2023-12-01 17:54:25 -0500

    Update create_config.R

--- create_config.R
+++ create_config.R
@@ -1,10 +1,12 @@
 print("create_config.R")
 
-library(gen3sis)
+file.copy("config.R", "/pwd/config.R", overwrite=TRUE)
+
+# library(gen3sis)
 
 # get data path
 # datapath <- system.file(file.path("extdata", "EmptyConfig"), package="gen3sis")
 # set config_empty.R file path
-config_file_path <- file.path("/pwd/config", "config.R")
+# config_file_path <- file.path("/pwd/config", "config.R")
 
-write_config_skeleton(config_file_path)
+# write_config_skeleton(config_file_path)

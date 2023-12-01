23d735c97329662bf33ab0e49dd8e37065b5c8a5
Matthew Andres Moreno <mmore500.login@gmail.com>
2023-12-01 16:57:58 -0500

    Create run_simulation.R

---
+++ run_simulation.R
@@ -0,0 +1,12 @@
+library(gen3sis)
+print(paste("gen3sis version:", packageVersion("gen3sis")))
+
+print("run_simiulation.R")
+
+sim <- run_simulation(
+    config = file.path("/pwd/config", "config.R"), 
+    landscape = file.path("/pwd/config", "landscape"),
+    output_directory = "/pwd/data",
+    call_observer = 1,
+    verbose=0
+)
